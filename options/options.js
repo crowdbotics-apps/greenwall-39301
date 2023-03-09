36d39e794ae3312abceeda7568ac32e7b3f0f605
Tulio Fernandes <tfernandes@qmo.io>
2023-03-09 12:10:56 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "greenwall-39301",
-  url: "https://greenwall-39301.botics.co",
-  api: "https://greenwall-39301.botics.co/api/v1"
-}
+  "name": "greenwall-39301",
+  "url": "https://greenwall-39301.botics.co",
+  "api": "https://greenwall-39301.botics.co/api/v1",
+  "initialRoute": "login"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
-    copy: "Routes available!"
+    "copy": "Routes available!"
   }
-}
+};
